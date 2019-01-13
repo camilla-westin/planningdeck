732eafde6cf190c2f07a2c2acd37971f886be09f
Camilla Westin <camilla@webstin.se>
2019-01-13 21:14:26 +0100

    Added reload button. Responsive styling.

--- src/components/Deck.vue
+++ src/components/Deck.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="deck">
       <div class="cards">
-        <div v-for="card in cards" :key="card.index" v-on:click="selectCard(card)" class="card-wrap" v-bind:class="{'selected' : card.selected }"> 
+        <div v-for="card in cardsList" :key="card.index" v-on:click="selectCard(card)" class="card-wrap" v-bind:class="{'selected' : card.selected }"> 
             <transition name="flip">
                 <div v-if="card.revealed" class="card card-front">
                     <span class="complexity">{{ card.complexity }}</span>
@@ -12,58 +12,60 @@
             </transition>
         </div>
       </div>
+      <button id="restart" v-on:click="reLoad()">Start over</button>
   </div>
 </template>
 
 <script>
+var cards = 
+[
+    { complexity: '0', revealed: true, selected: false } ,
+    { complexity: '1', revealed: true, selected: false } ,
+    { complexity: '2', revealed: true, selected: false },
+    { complexity: '3', revealed: true, selected: false },
+    { complexity: '5', revealed: true, selected: false },
+    { complexity: '8', revealed: true, selected: false },
+    { complexity: '13', revealed: true, selected: false },
+    { complexity: '20', revealed: true, selected: false },
+    { complexity: '40', revealed: true, selected: false },
+    { complexity: '100', revealed: true, selected: false },
+    { complexity: '?', revealed: true, selected: false },
+    { complexity: '∞', revealed: true, selected: false }
+]
 
 export default {
   name: 'Deck',
   data() {
       return {
-          cards: [
-            { complexity: '0', revealed: true, selected: false } ,
-            { complexity: '1', revealed: true, selected: false } ,
-            { complexity: '2', revealed: true, selected: false },
-            { complexity: '3', revealed: true, selected: false },
-            { complexity: '5', revealed: true, selected: false },
-            { complexity: '8', revealed: true, selected: false },
-            { complexity: '13', revealed: true, selected: false },
-            { complexity: '20', revealed: true, selected: false },
-            { complexity: '40', revealed: true, selected: false },
-            { complexity: '100', revealed: true, selected: false },
-            { complexity: '?', revealed: true, selected: false },
-            { complexity: '∞', revealed: true, selected: false }
-        ]
+          cardsList: cards
       }
   },
   methods: {
       selectCard: function(card) {
-        //Hide all cards
-        var cardElements = document.getElementsByClassName('card-wrap')
 
-        for (var i = 0; i < cardElements.length; ++i) {
-            cardElements[i].classList.add('hidden');
+        //Hide all cards
+        var cardElement = document.getElementsByClassName('card-wrap');
+
+        for (var i = 0; i < cardElement.length; ++i) {
+            cardElement[i].classList.add('hidden');
         }
 
-        //Change class on clicked card to selected
+        //Change class on clicked card to selected to show it
         card.selected = !card.selected;
 
-        // Flip card and show backside
+        // Toggle card. When first selected it will show backside then it can be toggled.
         card.revealed = !card.revealed;
-        
-        //If card has .selected class
-        if(card.classList.contains('selected')) {
-            // Show selected card
-            card.style.display = 'block';
-            
-            //Reveal card on click if card is already selected.
-            card.revealed = !card.revealed;
 
-            //Todo: Back button
-        }
+        //Show restart button
+        document.getElementById('restart').style.display = 'block';            
+      },
+      reLoad: function() {
+        //Reload page to start again  
+        window.location.reload();
       }
   }
+
+
 }
 </script>
 
